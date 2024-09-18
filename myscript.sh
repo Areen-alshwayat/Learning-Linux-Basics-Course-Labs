8608e3ee272b75f365fab30ec2dfc33b0cccace6
Areen <61476000+Areen-alshwayat@users.noreply.github.com>
2024-09-18 18:30:49 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # This is a simple shell script
 echo "Hello, World!"
+
+#!/bin/bash
+# This script prints a message and shows the current date and time
+
+echo "Hello, welcome to my script!"
+echo "Current date and time: $(date)"
